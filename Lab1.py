46334cf39cb42874bd82e9baa04b522be75b52e9
Oxygen033 <102524912+Oxygen033@users.noreply.github.com>
2023-03-22 15:29:16 +0600

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
+import requests
 
 url = "https://www.omgtu.ru"
+page= requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 file = open("output.txt", "w")
 while True:
